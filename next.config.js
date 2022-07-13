516b492577cd5977311e61db2f05dbef5fbe9529
Michal Miszczyszyn <michal@mmiszy.pl>
2022-07-13 16:48:57 +0200

    Bug repro

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import("next").NextConfig} */
 const config = {
   reactStrictMode: true,
+  i18n: {
+		defaultLocale: 'pl',
+		locales: ['pl', 'en'],
+	},
 }
 
 module.exports = config
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,30 @@
-/** Add your relevant code here for the issue to reproduce */
-export default function Home() {
-  return null
+import { GetStaticPropsContext, GetStaticPropsResult, InferGetStaticPropsType } from "next";
+import Link from "next/link";
+import { useRouter } from "next/router";
+
+export default function Home({someValueFromTheServer}: InferGetStaticPropsType<typeof getStaticProps>) {
+  const { locales, pathname, asPath, query } = useRouter();
+
+  return (
+    <footer>
+      <p>{someValueFromTheServer}</p>
+      <ul>
+        {locales?.map((l) => (
+          <li key={l}>
+            <Link locale={l} href={{ pathname, query }} as={asPath}>
+              <a>{l}</a>
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </footer>
+  );
+}
+
+export const getStaticProps = (ctx: GetStaticPropsContext) => {
+  return {
+    props: {
+      someValueFromTheServer: ctx.locale || ctx.defaultLocale,
+    }, revalidate: 60,
+  }
 }
