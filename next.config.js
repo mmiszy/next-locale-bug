e02da91a03c21f67941c4390c2f5a9a8707e99f9
Michal Miszczyszyn <michal@mmiszy.pl>
2022-07-13 17:25:12 +0200

    10

--- next.config.js
+++ next.config.js
@@ -4,11 +4,7 @@ const config = {
   i18n: {
     defaultLocale: "pl",
     locales: ["pl", "en"],
-  },
-  experimental: {
-    runtime: "experimental-edge",
-  },
-  trailingSlash: true,
+  }
 };
 
 module.exports = config;
