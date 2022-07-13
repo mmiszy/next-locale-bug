f7d6e0cb437dae64239ef258d35a20beb435b7ce
Michal Miszczyszyn <michal@mmiszy.pl>
2022-07-13 16:57:57 +0200

    5

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import("next").NextConfig} */
 const config = {
   reactStrictMode: true,
+  excludeDefaultMomentLocales: true,
+  images: {
+    domains: ["typeofweb.eu.saleor.cloud"],
+    formats: ["image/avif", "image/webp"],
+  },
+  experimental: { images: { allowFutureImage: true } },
   i18n: {
     defaultLocale: "pl",
     locales: ["pl", "en"],
